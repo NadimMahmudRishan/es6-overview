75c655f73b023c636e623f6360f4d4113039a523
Rishan <98330109+NadimMahmudRishan@users.noreply.github.com>
2024-10-15 20:31:04 +0600

    Add some basic CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Sunglass from "./components/sunglass/sunglass";
+import Watch from "./components/watch/Watch";
 function App() {
+  const [watches, setWatches] = useState([]);
+  useEffect(() => {
+    fetch("watches.json")
+      .then((res) => res.json())
+      .then((data) => setWatches(data));
+  }, []);
   return (
     <>
-      <div>
+      {/* <div>
         <p>Nadim mahmud rishan</p>
         <Sunglass></Sunglass>
+      </div> */}
+
+      <div>
+        {watches.map((watch) => (
+          <Watch key={watch.id} watch={watch}></Watch>
+        ))}
+        ;
       </div>
     </>
   );
--- src/components/watch/watch.jsx
+++ src/components/watch/watch.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import './watch.css';
 
-const Watch = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const Watch = ({ watch }) => {
+  const { brand, model, price } = watch;
+  return (
+    <div className="box">
+      <h3>Brand : {brand}</h3>
+      <p>Model : {model}</p>
+      <p>Price : {price}</p>
+    </div>
+  );
 };
 
 export default Watch;
